c7b65257987f79cff96e6d3b6d2243cba829c4ab
FlutLab.io <>
2022-02-14 19:57:54 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navigation Over Screens',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+//      home: MainPage(),
+
+      // Declare routes
+      routes: {
+        // Main initial route
+        '/': (context) => MainPage(),
+        // Second route
+        '/second': (context) => SecondPage(),
+      },
+      initialRoute: '/',
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Nation over screens'),
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              // Navigate using declared route name
+              ElevatedButton(
+                onPressed: () => Navigator.pushNamed(context, '/second'),
+                child: Text('Naviate using routes'),
+              ),
+              // Navigate using simple push method
+              ElevatedButton(
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondPage()),
+                ),
+                child: Text('Nvigate using push ethod'),
+              )
+            ],
+          ),
+        ),
+      );
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sed screen'),
+      ),
+      body: Container(
+        child: ElevatedButton(
+          onPressed: () => Navigator.pop(context),
+          child: Text('Back'),
+        ),
+      ),
+    );
+  }
+}
